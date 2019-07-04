b87cdd4690f3089c88a4b66bb964f1990b6f8ee9
v-sokolov <v-sokolov@futuro.team>
2019-07-04 18:54:25 +0300

    input's validation, canvas draw, misc fixes

--- httpRequestsProject/index.js
+++ httpRequestsProject/index.js
@@ -9,8 +9,10 @@ function getFromServer() {
 }
 // Function for showing "something" (including) in the html-object
 function showSomething(data) {
-    data.forEach(element => {
-        $(".list-box").append(`<li id=${element.id}>ID:  ${element.id} Value:    ${element.value}</li>`);
+    data.forEach((element) => {
+        $(".list-box").append(`<li id=${element.id}><button onclick="deleteFromServer(${element.id})">del</button>
+                                                    ID: ${element.id}
+                                                    Value: ${element.value}</li>`);
     });
 }
 //Creating onclik-event for the button SEND
@@ -22,9 +24,10 @@ function postNewValueToServer(){
     axios.post('http://192.168.1.100:3000/api/items', {
         value: $("#new").val()
     }).then((answer)=>{
-        // console.log(answer);
         $("#new").val('');
-        $(".list-box").append(`<li id=${answer.data.id}>ID ${answer.data.id} Value ${answer.data.value}</li>`);
+        $(".list-box").append(`<li id=${answer.data.id}><button onclick="deleteFromServer(${answer.data.id})">del</button>
+                                                        ID: ${answer.data.id}
+                                                        Value: ${answer.data.value}</li>`);
     }).catch((error)=>{
         alert(error);
     })
@@ -34,12 +37,12 @@ $("#del").click(function() {
     deleteFromServer();
 });
 //Deleting an object from the server
-function deleteFromServer(){
-    axios.delete(`http://192.168.1.100:3000/api/items/${$("#old").val()}`)
+function deleteFromServer(data){
+    axios.delete(`http://192.168.1.100:3000/api/items/${data}`)
     .then((answer)=>{
-        $("#old").val('');
         $(`#${answer.data.id}`).remove();
-    }).catch((error)=>{
+    })
+    .catch((error)=>{
         console.log(error);
     })
 }
@@ -47,6 +50,48 @@ function deleteFromServer(){
 $("#ve").click(function() {
     postNewValueEmailToServer();
 });
+//Validation for inputs
+
+//my regular expression for email
+    let x = /.+@.+\..+/i;
+// disable buttons by default
+    $("#send").prop('disabled', true);
+    $("#ve").prop('disabled', true);
+    $("#draw").prop('disabled', false);
+//validation and outline new-input
+    $("#new").on("input", function() {
+        if($(this).val().trim()) {
+            $("#new").css("outline-color","green");
+            $("#send").prop('disabled', false);
+        } else {
+            $("#send").prop('disabled', true);
+            $("#new").css("outline-color","brown");
+        }
+    })
+// validation on empty string + outline
+    $("#value").on("input", function() {
+        if($("#value").val().trim()){
+            $("#value").css("outline-color","green");
+        } else {
+            $("#value").css("outline-color","brown");
+        }
+    })
+// validation on empty string + outline
+    $("#email").on("input", function() {
+        if($("#email").val().trim()&&x.test($("#email").val())){
+            $("#email").css("outline-color","green");
+        } else {
+            $("#email").css("outline-color","brown");
+        }
+    })
+// Validation value & mail inputs for valid
+    $("#value, #email").on("input", function() {
+        if($("#value").val().trim()&&$("#email").val().trim()&&x.test($("#email").val())) {
+            $("#ve").prop('disabled', false);
+        } else {
+            $("#ve").prop('disabled', true);
+        }
+    })
 //Sending to server value and email of input's textareas
 function postNewValueEmailToServer(){
     axios.post(`http://192.168.1.100:3000/api/contact`, {
@@ -54,10 +99,71 @@ function postNewValueEmailToServer(){
         email: $("#email").val()
     })
     .then((answer)=>{
+        $("#value, #email").val('');
         $(".message").append(`<p> ${answer.data}</p>`);
         $(".message").css("display","flex");
     }).catch((error)=>{
         alert(error);
     })
 }
+//Get array of points from the server
+function getChart(){
+    axios.get('http://192.168.1.100:3000/api/chart')
+    .then((content)=>{
+        return content.data;
+    })
+    .then((data)=>{
+        let arrayX=data.map(item=>item[0]);
+        let arrayY=data.map(item=>item[1]);
+        var charts = document.getElementById("charts"),
+        ctx= charts.getContext('2d');
+        ctx.beginPath();
+        ctx.lineWidth = "2";
+        ctx.strokeStyle="#ff33cc";
+        ctx.translate(charts.width/2,charts.height/2);
+        ctx.moveTo(arrayX[0]*7,-arrayY[0]*7)
+        for (i=1; i<arrayX.length; i++){
+            ctx.lineTo(arrayX[i]*7, -arrayY[i]*7);
+            }
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.lineWidth = "1";
+        ctx.strokeStyle="#003300";
+        ctx.moveTo(0, -charts.height/2)
+        ctx.lineTo(0, charts.height/2)
+        ctx.moveTo(-charts.width/2,0)
+        ctx.lineTo(charts.width/2,0)
+        ctx.stroke();
+
+        ctx.lineWidth = "1";
+        ctx.strokeStyle="#003300";
+        ctx.moveTo(0,-charts.height/2)
+        ctx.lineTo(-4,-charts.height/2+10)
+        ctx.moveTo(0,-charts.height/2)
+        ctx.lineTo(4,-charts.height/2+10)
+        ctx.stroke();
+
+        ctx.lineWidth = "1";
+        ctx.strokeStyle="#003300";
+        ctx.moveTo(charts.width/2,0)
+        ctx.lineTo(charts.width/2-10,4)
+        ctx.moveTo(charts.width/2,0)
+        ctx.lineTo(charts.width/2-10,-4)
+        ctx.stroke();
+        ctx.closePath();
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+//Creating onclik-event for the button draw canvas
+$("#draw").click(function() {
+    getChart();
+    $("#draw").prop('disabled', true);
+});
+
+// Starting main method
 getFromServer();
+// getChart();
+// draw();
