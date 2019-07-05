7c61d15a0a91ba76f584308e1dfafe1d67a7eaad
v-sokolov <v-sokolov@futuro.team>
2019-07-05 18:56:07 +0300

    custom color-switcher (fixes)

--- js misc/custom-jQuerry-method/index.js
+++ js misc/custom-jQuerry-method/index.js
@@ -1,5 +1,8 @@
-// extending default jQuery by new method
+//extending default jQuery by new method
+
+//adding new method to default method's prototype
 jQuery.prototype.colorMagic = function(r,g,b,x) {
+    // setting background-color from entered rgb-color
     this.css("background-color", `rgb(${r},${g},${b})`);
     if(x>0) {
         r<x ? r=0 : r=r-x;
@@ -10,6 +13,7 @@ jQuery.prototype.colorMagic = function(r,g,b,x) {
         g-x<256 ? g=g-x : g=255;
         b-x<256 ? b=b-x : b=255;
     }
+    // setting text-color from entered (rgb-color minus x-value)
     this.css("color", `rgb(${r},${g},${b})`);
     x=-x;
     if(x>0) {
@@ -21,7 +25,66 @@ jQuery.prototype.colorMagic = function(r,g,b,x) {
         g-x<256 ? g=g-x : g=255;
         b-x<256 ? b=b-x : b=255;
     }
-    this.css("border-color", `rgb(${r},${g},${b})`);
+    // setting border-color from entered (rgb-color->brg plus x-value)
+    this.css("border-color", `rgb(${b},${r},${g})`);
 }
-// appling this method on <p> with default (#000)-color
-$("#test").colorMagic(153,255,153,85);
+//disabling button MAGIC by default
+$("#switch").prop('disabled', true);
+$("#reset").prop('disabled', false);
+//follow refreshing input's textareas
+//validation input's textareas on empty-string and numbers
+$("#r, #g, #b, #x").on("input", function() {
+    if($("#r").val().trim() && Number($("#r").val())) {
+        $("#r").css("outline-color","green");
+        $("#switch").prop('disabled', false);
+    } else {
+        $("#r").css("outline-color","brown");
+        $("#switch").prop('disabled', true);
+    }
+    if($("#g").val().trim() && Number($("#g").val())) {
+        $("#g").css("outline-color","green");
+        $("#switch").prop('disabled', false);
+    } else {
+        $("#g").css("outline-color","brown");
+        $("#switch").prop('disabled', true);
+    }
+    if($("#b").val().trim() && Number($("#b").val())) {
+        $("#b").css("outline-color","green");
+        $("#switch").prop('disabled', false);
+    } else {
+        $("#b").css("outline-color","brown");
+        $("#switch").prop('disabled', true);
+    }
+    if($("#x").val().trim() && Number($("#x").val())) {
+        $("#x").css("outline-color","green");
+        $("#switch").prop('disabled', false);
+    } else {
+        $("#x").css("outline-color","brown");
+        $("#switch").prop('disabled', true);
+    }
+})
+//click event on SWITCH button which CHANGING color by custom method
+$("#switch").click(function() {
+    $("#test").colorMagic($("#r").val(),$("#g").val(),$("#b").val(),x);
+    $("#r, #g, #b, #x").val('');
+    $("#switch").prop('disabled', true);
+    $("#switch").css("background-color","gold")
+    $("#switch").css("color","black")
+    setTimeout(() => {
+        $("#switch").css("background-color","azure");
+        $("#switch").css("color","green");
+    }, 1000);
+});
+//click event on SWITCH button which CHANGING color by default
+$("#reset").click(function() {
+    $("#r, #g, #b, #x").val('');
+    $("#test").css("background-color","#fff")
+    $("#test").css("color","#000")
+    $("#test").css("border-color","#000")
+    $("#reset").css("background-color","lightgray")
+    $("#reset").css("color","black")
+    setTimeout(() => {
+        $("#reset").css("background-color","azure");
+        $("#reset").css("color","green");
+    }, 200);
+});
