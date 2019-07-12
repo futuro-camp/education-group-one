557e2dcc10f4d88c4ec541d6c37f783a0755a0c8
Vadim <v.zinchenko@futuro.team>
2019-07-12 19:59:58 +0300

    Implemented shopX Card application on React
    Added Card item and form fro adding and removing it from page

---
+++ React/shopx_react/src/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import './App.css';
+import Card from './components/Card'
+import cardImage from './image/cardImage.jpg'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { cards: [], id: 0 }
+    this.addCard = this.addCard.bind(this)
+    this.removeCard = this.removeCard.bind(this);
+  }
+
+  addCard() {
+    this.setState(
+      { 
+        cards: [
+          ...this.state.cards, 
+          <Card 
+            key = {this.state.id}
+            cardImage = {cardImage} 
+            cardName = {this.name.value} 
+            price = {{
+              old: "$"+this.oldPrice.value,
+              current: "$"+this.currentPrice.value 
+            }}
+            deleteMethod = {this.removeCard(this.state.id)}
+          />
+        ], 
+        id: this.state.id + 1 
+      }
+    )
+  }
+
+  removeCard(cardKey) {
+    return () => {
+      let cardsArray = this.state.cards
+      cardsArray.splice(this.state.cards.map(x => x.key).indexOf(cardKey), 1);
+      this.setState({ cards: cardsArray, id: this.state.id })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="inputs">
+          <input className="valueInput" ref={(input)=>{this.name = input}} type="text" placeholder="Card Name"/>
+          <input className="valueInput" ref={(input)=>{this.oldPrice = input}} type="number" placeholder="Card Old Price"/>
+          <input className="valueInput" ref={(input)=>{this.currentPrice = input}} type="number" placeholder="Card New Price"/>
+          <input className="submitInput" onClick={this.addCard} type="submit" value="Add Card"/>
+        </div>
+        <div className="cards">
+          {this.state.cards}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ React/shopx_react/src/components/Card.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react'
+import addToCompareListIcon from '../icons/addToCompareList.svg'
+import likeIcon from '../icons/likeIcon.svg'
+import filterIcon from '../icons/filterIcon.svg'
+import cartIcon from '../icons/cartIcon.svg'
+import './CardStyle.css'
+
+
+function CardMenuButton(props) {
+    return (
+        <button>
+            <img src={props.icon} alt="Menu Button"/>
+        </button>
+    )
+}
+
+class CardMenuLikeButton extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { liked: false }
+    }
+
+    render() {
+        return (
+            <button>
+                <img src={ likeIcon } alt="Like"/>
+            </button>
+        )
+    }
+}
+
+function CardMenu(props) {
+    return(
+        <div className="cardMenu">
+            <CardMenuButton icon = {filterIcon}/>
+            <CardMenuLikeButton/>
+            <CardMenuButton icon = {addToCompareListIcon}/>
+        </div>
+    )
+}
+
+function CardImage(props) {
+    return <img className="cardImage" src={props.cardImage} alt="CardImage"/>
+}
+
+function CardHeader(props) {
+    return (
+        <div className="cardHeader">
+            <CardMenu/>
+            <CardImage cardImage={props.cardImage}/>
+        </div>
+    )
+}
+
+function CardContent(props) {
+    return (
+        <div className="cardContent">
+            <h1 className="heading">{props.cardName}</h1>
+            <p className="currentPrice"><span className="oldPrice">{props.price.old}</span>{props.price.current}</p>
+        </div>
+    )
+}
+
+function CardFooter(props) {
+    return (
+        <button className="cardFooter">
+            <img src={cartIcon} alt="Cart"/>
+            <p>Add To cart</p>
+        </button>
+    )
+}
+
+function CardDeleteButton(props) {
+    return <button onClick = {props.deleteMethod} className="removeButton">Remove</button>
+}
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                <CardHeader cardImage={this.props.cardImage}/>
+                <CardContent cardName={this.props.cardName} price={this.props.price}/>
+                <CardFooter/>
+                <CardDeleteButton deleteMethod={this.props.deleteMethod}/>
+            </div>
+        )
+    }
+}
