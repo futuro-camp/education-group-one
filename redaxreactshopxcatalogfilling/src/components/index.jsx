7c9dd3454c31226ea4e759555db5d1ea947cb9f5
v-sokolov <v-sokolov@futuro.team>
2019-07-19 15:07:22 +0300

    Styled Card-Component
    implemented styles and html from 
    original shopX-project

--- redaxreactshopxcatalogfilling/src/components/index.jsx
+++ redaxreactshopxcatalogfilling/src/components/index.jsx
@@ -1,14 +1,48 @@
 import React from "react";
+import "./index.css";
 import {connect} from "react-redux";
 import { removeItem } from "../actions";
 
 function Card(props) {
     //We assign a function on event-onClick, which calls another function(id) which do "removing"
     return (
-        <div>
-            <li key={props.id} id={props.id}>{props.name} {props.price} {props.discount}</li>
-            <button onClick={function() {props.onClickRemove(props.id)}}>Remove</button>
+        // <div className="card">
+        //     <li key={props.id} id={props.id}>{props.name} {props.price} {props.discount}</li>
+        //     <button onClick={function() {props.onClickRemove(props.id)}}>Remove</button>
+        // </div>
+        <div className="card">
+        <div className="header">
+            <div className="icons">
+                <button id="list"></button>
+                <button id="like"></button>
+                <button id="compare"></button>
+            </div>
+            <div className="photo">
+            </div>
         </div>
+        <div className="sectionBody">
+            <div className="text">
+                <p className="itemTitle">{props.name}</p>
+                <div className="priceRow">
+                    <p className="itemOldPrice">{props.price}$</p>
+                    <p className="itemNewPrice">    {props.discount}$</p>
+                </div>
+            </div>
+        </div>
+        <div className="footer">
+            <button className="addToCart">
+                <div className="basketImg"></div>
+                <div className="buttonTitle">
+                    <p>Add to cart</p>
+                </div>
+            </button>
+            <button className="removeFromCart" onClick={function() {props.onClickRemove(props.id)}}>
+                <div className="buttonTitle">
+                    <p>Remove</p>
+                </div>
+            </button>
+        </div>
+    </div>
     )
 }
 
