2dacd87b9ba98d7053f024fa6ba271709725d3a8
v-sokolov <v-sokolov@futuro.team>
2019-08-28 14:19:54 +0300

    style fixes

--- animecatalog/src/components/singleAnime.tsx
+++ animecatalog/src/components/singleAnime.tsx
@@ -21,7 +21,9 @@ const SingleAnime = (props:any) => {
     return (
         <div className="SingleAnime">
             {/* <h1>SINGLE ANIME </h1> */}
-            {id? <img className="cover" src={attributes.coverImage.large}/> : <h2>none</h2> }
+            {attributes?
+                attributes.coverImage? <img className="cover" src={attributes.coverImage.large}/> : <img className="cover" src="" alt="no image" /> : <img className="cover" src="" alt="no image" />
+            }
             {attributes?
                 <div className="info">
                     <div className="image">
@@ -29,22 +31,24 @@ const SingleAnime = (props:any) => {
                     </div>
 
                     <div className="description">
-                        <h1>{attributes.titles.en}&emsp; <span>{attributes.startDate.split("-")[0]}</span></h1>
-                        <h4>{attributes.averageRating}% &emsp;Community Approval</h4>
+                        <h1>{attributes.canonicalTitle}&emsp;
+                            {attributes.startDate? <span>{attributes.startDate.split("-")[0]}</span> : <span></span> }
+                        </h1>
+                        {attributes.averageRating? <h4> {attributes.averageRating}% &emsp;Community Approval</h4> : <h4></h4> }
                         <span> {attributes.ageRatingGuide} </span>
                         <p>{attributes.synopsis}</p>
                         <h6></h6>
                     </div>
 
                     <ul className="details">
-                        <li>Status: <span>{attributes.status}</span></li>
-                        <li>Show Type: <span>{attributes.showType}</span></li>
-                        <li>Episode Count: <span>{attributes.episodeCount}</span></li>
-                        <li>Episode Length: <span>{attributes.episodeLength}</span></li>
-                        <li>Favorite Count: <span>{attributes.favoritesCount}</span></li>
-                        <li>Total Length: <span>{attributes.totalLength}</span></li>
-                        <li>User Count: <span>{attributes.userCount}</span></li>
-                        <li>Rating Rank: <span>{attributes.ratingRank}</span></li>
+                        { attributes.status? <li>Status:<span>  {attributes.status}</span></li> : <li></li> }
+                        { attributes.showType? <li>Show Type:<span>  {attributes.showType}</span></li> : <li></li> }
+                        { attributes.episodeCount? <li>Episode Count:<span>  {attributes.episodeCount}</span></li> : <li></li> }
+                        { attributes.episodeLength? <li>Episode Length:<span>  {attributes.episodeLength}</span></li> : <li></li> }
+                        { attributes.favoritesCount? <li>Favorite Count:<span>  {attributes.favoritesCount}</span></li> : <li></li> }
+                        { attributes.totalLength? <li>Total Length:<span>  {attributes.totalLength}</span></li> : <li></li> }
+                        { attributes.userCount? <li>User Count:<span>  {attributes.userCount}</span></li> : <li></li> }
+                        { attributes.ratingRank? <li>Rating Rank:<span>  {attributes.ratingRank}</span></li> : <li></li> }
                     </ul>
                 </div>: <div></div> }
         </div>
